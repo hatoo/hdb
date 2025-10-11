c36cf42115818a9ba17fb19157e881f54f560e34
hatoo <hato2000@gmail.com>
2025-10-11 17:36:10 +0900

    a

--- src/stop_reason.rs
+++ src/stop_reason.rs
@@ -8,7 +8,11 @@ impl std::fmt::Display for StopReason {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             StopReason::SysCall(num) => {
-                write!(f, "{:?}", syscall_numbers::native::sys_call_name(*num))
+                write!(
+                    f,
+                    "{}",
+                    syscall_numbers::native::sys_call_name(*num).unwrap_or("unknown syscall")
+                )
             }
             StopReason::Other(status) => write!(f, "{:?}", status),
         }
